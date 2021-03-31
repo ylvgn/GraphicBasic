d862efcfc955c7d391fc269c9f3efdfdd3d4545e
gyh <gyhguanyonghua@163.com>
2021-03-31 23:16:24 +0800

    draw line

--- Assets/Scripts/MyGUI.cs
+++ Assets/Scripts/MyGUI.cs
@@ -7,7 +7,6 @@ using UnityEngine;
 public class MyGUI : MonoBehaviour
 {
     static MyGUI instance;
-
     public enum Task
     {
         Rect,
@@ -50,18 +49,23 @@ public class MyGUI : MonoBehaviour
         switch (MyEnum)
         {
             case Task.Rect:
-                DrawRect(instance.tex, 30, 60, 256, 256, new Color(0, 1, 0, 1));
+                DrawRect(tex, 30, 60, 256, 256, new Color(0, 1, 0, 1));
                 break;
             case Task.RectOutLine:
-                DrawRectOutLine(instance.tex, 30, 60, 256, 256, new Color(1, 1, 1, 1));
+                DrawRectOutLine(tex, 30, 60, 256, 256, new Color(1, 1, 1, 1));
                 break;
             case Task.RectOutLineAndRect:
-                DrawRect(instance.tex, 30, 60, 256, 256, new Color(0, 1, 0, 1));
-                DrawRectOutLine(instance.tex, 30, 60, 256, 256, new Color(1, 1, 1, 1));
+                DrawRect(tex, 30, 60, 256, 256, new Color(0, 1, 0, 1));
+                DrawRectOutLine(tex, 30, 60, 256, 256, new Color(1, 1, 1, 1));
                 break;
             case Task.Line:
+                DrawLine(tex, 10, 50, 10, 320, new Color(0, 1, 1, 1));  // k 不存在
+                DrawLine(tex, 10, 20, 200, 20, new Color(0, 1, 0, 1));  // k == 0
+                DrawLine(tex, 30, 30, 350, 100, new Color(1, 1, 0, 1)); // k > 0
+                DrawLine(tex, 30, 400, 300, 60, new Color(1, 1, 1, 1)); // k < 0
                 break;
             case Task.Line_no_float:
+                DrawLineNF(tex, 30, 400, 300, 60, new Color(1, 1, 1, 1)); // k < 0
                 break;
             case Task.Circle:
                 break;
@@ -70,12 +74,21 @@ public class MyGUI : MonoBehaviour
             case Task.Triangle:
                 break;
             case Task.TriangleOutLine:
+                DrawTriangle(tex, 10, 10, 150, 150, 200, 10, new Color(0, 1, 0, 1));
                 break;
             default:
                 break;
         }
 
         tex.Apply(false);
+    }
+
+    private void DrawTriangle(Texture2D tex, int x1, int y1, int x2, int y2, int x3, int y3, Color color)
+    {
+        // TODO 可能是无效的三角形
+        DrawLine(tex, x1, y1, x2, y2, color);
+        DrawLine(tex, x1, y1, x3, y3, new Color(1,1,1,1));
+        DrawLine(tex, x2, y2, x3, y3, new Color(1, 0, 1, 1));
     }
 
     private void DrawRectOutLine(Texture2D tex, int x, int y, int w, int h, Color color)
@@ -86,14 +99,10 @@ public class MyGUI : MonoBehaviour
             return;
         }
 
-        for (int i = 0; i < w; i ++) // bottom
-            tex.SetPixel(i + x, y, color);
-        for (int j = 0; j < h; j ++) // left
-            tex.SetPixel(x, j + y, color);
-        for (int i = 0; i < w; i ++) // top
-            tex.SetPixel(i + x, y + h, color);
-        for (int j = 0; j < h; j++) // right
-            tex.SetPixel(w + x, j + y, color);
+        for (int i = 0; i < w; i ++) tex.SetPixel(i + x, y, color);     // bottom
+        for (int j = 0; j < h; j ++) tex.SetPixel(x, j + y, color);     // left
+        for (int i = 0; i < w; i ++) tex.SetPixel(i + x, y + h, color); // top
+        for (int j = 0; j < h; j ++) tex.SetPixel(w + x, j + y, color); // right
     }
 
     void DrawRect(Texture2D tex, int x, int y, int w, int h, Color color)
@@ -107,6 +116,49 @@ public class MyGUI : MonoBehaviour
         for (int i = 0; i < w; i++)
             for (int j = 0; j < h; j++)
                 tex.SetPixel(i + x, j + y, color);
+    }
+
+    void DrawLine(Texture2D tex, int x1, int y1, int x2, int y2, Color color)
+    {
+        if (x1 < 0 || y1 < 0 || x2 < 0 || y2 < 0 || x1 >= tex.width || y1 >= tex.height || x2 >= tex.width || y2 >= tex.height)
+        {
+            Debug.LogError("[DrawLine] args error");
+            return;
+        }
+
+        if (x2 == x1)
+        {
+            int x = Mathf.Min(x1, x2);
+            int y = Mathf.Min(y1, y2);
+            DrawRect(tex, x, y, 1, Mathf.Max(y1, y2) - y, color);
+            return;
+        }
+
+        float k = (y2 - y1) * 1.0f / (x2 - x1);
+        if (k == 0)
+        {
+            int x = Mathf.Min(x1, x2);
+            int y = Mathf.Min(y1, y2);
+            DrawRect(tex, x, y, Mathf.Max(x1, x2) - x, 1, color);
+            return;
+        }
+
+        // y - y0 = k * (x - x0)
+        for (int x = x1; x <= x2; x ++)
+        {
+            int y = MyUtility.RoundToInt(k * (x - x1) + y1);
+            tex.SetPixel(x, y, color);
+        }
+    }
+
+    void DrawLineNF(Texture2D tex, int x1, int y1, int x2, int y2, Color color)
+    {
+        if (x1 < 0 || y1 < 0 || x2 < 0 || y2 < 0 || x1 >= tex.width || y1 >= tex.height || x2 >= tex.width || y2 >= tex.height)
+        {
+            Debug.LogError("[DrawLineNF] args error");
+            return;
+        }
+
     }
 
     void OnGUI()
---
+++ Assets/Scripts/MyUtility.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MyUtility
+{
+    public static int RoundToInt(float x)
+    {
+        int res = (int)x;
+        float d = x - res;
+        if (d * 10 >= 5) return res + 1;
+        return res;
+    }
+}
