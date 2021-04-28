1314b6eec53d6d3ac515f7fb54a7ab82c77d3e5a
gyh <gyhguanyonghua@163.com>
2021-04-29 00:46:55 +0800

    line line cloest point 2

--- Assets/Scripts/part2/LineLineCloestPoint.cs
+++ Assets/Scripts/part2/LineLineCloestPoint.cs
@@ -9,7 +9,6 @@ public class LineLineCloestPoint : MonoBehaviour
     public GameObject p3;
     public GameObject p4;
 
-
     // 计算p投影到平面的p'
     Vector3 PointProject2Plane(Vector3 center, Vector3 nl, Vector3 p)
     {
@@ -27,14 +26,29 @@ public class LineLineCloestPoint : MonoBehaviour
         return o;
     }
 
-    void OnDrawGizmos()
+    void DrawPlane(Vector3 center, Vector3 nl, float size, Color c)
     {
-        // p1p2 -> ab, p3p4 -> cb
-        if (p1 == null || p2 == null || p3 == null || p4 == null) return;
-        var a = p1.transform.position;
-        var b = p2.transform.position;
-        var c = p3.transform.position;
-        var d = p4.transform.position;
+        var oldColor = Gizmos.color;
+        Gizmos.color = c;
+        int[] dx = new int[4] { 90, 0, -90, 0 };
+        int[] dz = new int[4] { 0, -90, 0, 90 };
+        Vector3 last = center;
+        Vector3 first = center;
+        for (int i = 0; i < 4; i++)
+        {
+            var rotation = Quaternion.Euler(dx[i], 0, dz[i]);
+            var t = rotation * nl;
+            var cur = center + t * size;
+            if (last != center) Gizmos.DrawLine(last, cur);
+            else first = cur;
+            last = cur;
+        }
+        Gizmos.DrawLine(last, first);
+        Gizmos.color = oldColor;
+    }
+
+    void ShortestLineBetweenTwoLinesIn3D(Vector3 a, Vector3 b, Vector3 c, Vector3 d)
+    {
         var ba = (a - b).normalized;
         var dc = (c - d).normalized;
         var nl = Vector3.Cross(dc, ba).normalized;
@@ -42,7 +56,7 @@ public class LineLineCloestPoint : MonoBehaviour
         // cd -> c'd', 令abc'd'在同一个平面上
         var c2planeAB = PointProject2Plane(a, nl, c); // c'
         var d2planeAB = PointProject2Plane(a, nl, d); // d'
-        
+
         // output
         var o_onAB = LineLineIntersectPoint(a, b, c2planeAB, d2planeAB); // o_onAB是直线c'd'和ab的交点
         Vector3 o_onCD;
@@ -78,17 +92,6 @@ public class LineLineCloestPoint : MonoBehaviour
         MyUtility.LogPoint(new Rect(0, 135, 100, 30), $"BA-Cross-DC= {nl}", Color.black, 20);
         MyUtility.LogPoint(new Rect(0, 160, 100, 30), $"==============================", Color.black, 20);
 
-        // debug: ab, cd
-        MyUtility.LogPoint(a, $"{p1.name}{a}", Color.white, 15);
-        MyUtility.LogPoint(b, $"{p2.name}{b}", Color.white, 15);
-        MyUtility.LogPoint(c, $"{p3.name}{c}", Color.white, 15);
-        MyUtility.LogPoint(d, $"{p4.name}{d}", Color.white, 15);
-        Gizmos.color = Color.red;
-        Gizmos.DrawLine(a, b);
-        Gizmos.color = Color.green;
-        Gizmos.DrawLine(c, d);
-        Gizmos.color = Color.white;
-
         // debug: c' d'
         Gizmos.DrawSphere(c2planeAB, 0.2f);
         MyUtility.LogPoint(c2planeAB, $"C'{c2planeAB}", Color.white, 15);
@@ -115,24 +118,49 @@ public class LineLineCloestPoint : MonoBehaviour
         //DrawPlane(c, nl, (c - d).magnitude + (b - a).magnitude, Color.green);
     }
 
-    void DrawPlane(Vector3 center, Vector3 nl, float size, Color c)
+    //dmnop = (xm - xn)(xo - xp) + (ym - yn)(yo - yp) + (zm - zn)(zo - zp)
+    float MyDot(Vector3 p1, Vector3 p2, Vector3 p3, Vector3 p4)
     {
-        var oldColor = Gizmos.color;
-        Gizmos.color = c;
-        int[] dx = new int[4] { 90, 0, -90, 0 };
-        int[] dz = new int[4] { 0, -90, 0, 90 };
-        Vector3 last = center;
-        Vector3 first = center;
-        for (int i = 0; i < 4; i++)
-        {
-            var rotation = Quaternion.Euler(dx[i], 0, dz[i]);
-            var t = rotation * nl;
-            var cur = center + t * size;
-            if (last != center) Gizmos.DrawLine(last, cur);
-            else first = cur;
-            last = cur;
-        }
-        Gizmos.DrawLine(last, first);
-        Gizmos.color = oldColor;
+        return (p1.x - p2.x) * (p3.x - p4.x) + (p1.y - p2.y) * (p3.y - p4.y) + (p1.z - p2.z) * (p3.z - p4.z);
+    }
+
+    // http://paulbourke.net/geometry/pointlineplane/
+    void ShortestLineBetweenTwoLinesIn3D2(Vector3 p1, Vector3 p2, Vector3 p3, Vector3 p4)
+    {
+        // mua = ( d1343 d4321 - d1321 d4343 ) / ( d2121 d4343 - d4321 d4321 )
+        var mua = (MyDot(p1, p3, p4, p3) * MyDot(p4, p3, p2, p1) - MyDot(p1, p3, p2, p1) * MyDot(p4, p3, p4, p3))
+            / (MyDot(p2, p1, p2, p1) * MyDot(p4, p3, p4, p3) - MyDot(p4, p3, p2, p1) * MyDot(p4, p3, p2, p1));
+
+        var mub = (MyDot(p1, p3, p4, p3) + mua * MyDot(p4, p3, p2, p1)) / MyDot(p4, p3, p4, p3);
+
+        //line = papb
+        var pa = p1 + mua * (p2 - p1);
+        var pb = p3 + mub * (p4 - p3);
+        var distance = (pb - pa).magnitude;
+        Gizmos.DrawLine(pa, pb);
+        MyUtility.LogPoint(new Rect(0, 200, 100, 30), $"outputDistance2={distance}", Color.black);
+    }
+
+    void OnDrawGizmos()
+    {
+        // p1p2 -> ab, p3p4 -> cb
+        if (p1 == null || p2 == null || p3 == null || p4 == null) return;
+        var a = p1.transform.position;
+        var b = p2.transform.position;
+        var c = p3.transform.position;
+        var d = p4.transform.position;
+        ShortestLineBetweenTwoLinesIn3D(a, b, c, d);
+        ShortestLineBetweenTwoLinesIn3D2(a, b, c, d);
+
+        // debug: ab, cd
+        MyUtility.LogPoint(a, $"{p1.name}{a}", Color.white, 15);
+        MyUtility.LogPoint(b, $"{p2.name}{b}", Color.white, 15);
+        MyUtility.LogPoint(c, $"{p3.name}{c}", Color.white, 15);
+        MyUtility.LogPoint(d, $"{p4.name}{d}", Color.white, 15);
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(a, b);
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(c, d);
+        Gizmos.color = Color.white;
     }
 }
